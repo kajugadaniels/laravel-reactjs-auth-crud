3b53d14147383e1a75abfda1ffdcd854e5fb32bf
kajugadaniels <kajugadaniels@gmail.com>
2024-07-03 18:58:34 +0200

    Context provider

--- client/src/components/DefaultLayout.jsx
+++ client/src/components/DefaultLayout.jsx
@@ -1,13 +1,29 @@
 import { Navigate, Outlet } from "react-router-dom";
+import { useStateContext } from "../contexts/ContextProvider";
 
 export default function DefaultLayout() {
+    const { token } = useStateContext();
+
+    if(!token) {
+        return <Navigate to='/login' />
+    }
 
     return (
-        <div>
-            <div>
-                Layout
+        <div id="defaultLayout">
+            <div className="content">
+                <header>
+                    <div>
+                        Header
+                    </div>
+                    <div>
+                        
+                        <a href="#" className="btn-logout"> Logout</a>
+                    </div>
+                </header>
+                <main>
+                    <Outlet />
+                </main>
             </div>
-            <Outlet />
         </div>
     )
 }
